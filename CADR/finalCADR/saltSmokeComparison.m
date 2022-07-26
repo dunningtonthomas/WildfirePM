6264a2c363f90ca67fcbc37c9e7c6bdccb3c6606
dunningtonthomas <76410702+dunningtonthomas@users.noreply.github.com>
2022-07-26 15:55:53 -0600

    Update

--- CADR/finalCADR/saltSmokeComparison.m
+++ CADR/finalCADR/saltSmokeComparison.m
@@ -10,7 +10,7 @@ load('CADR_Smoke.mat');
 figure();
 xStrings = ["Oransi", "Oreck", "Smoke Eater"];
 plotX = categorical(xStrings);
-yValues = [oraCFM, oreckCFM, seCFM];
+yValues = [oraCFM_Smoke, oreckCFM_Smoke, seCFM_Smoke];
 colors = {rgb('light blue'), rgb('light purple'), rgb('light orange')};
 
 %These are for the legends
@@ -18,24 +18,25 @@ scatter(plotX(1), 0, 'o', 'MarkerEdgeColor', 'k', 'MarkerFaceColor', 'k');
 hold on
 scatter(plotX(1), 0, 's', 'MarkerEdgeColor', 'k');
 
-scatter(plotX(1), oraM, 70, 'o', 'MarkerFaceColor', rgb('light blue'), 'MarkerEdgeColor', rgb('light blue'), 'linewidth', 1.25)
-scatter(plotX(3), seM, 70, 'o', 'MarkerFaceColor', rgb('light orange'), 'MarkerEdgeColor', rgb('light orange'), 'linewidth', 1.25)
-h1 = scatter(plotX(1), yValues(1), 100, 's', 'MarkerEdgeColor', colors{1}, 'linewidth', 1.25);
-h2 = scatter(plotX(2), yValues(2), 100, 's', 'MarkerEdgeColor', colors{2}, 'linewidth', 1.25);
-h3 = scatter(plotX(3), yValues(3), 100, 's', 'MarkerEdgeColor', colors{3}, 'linewidth', 1.25);
+scatter(plotX(1), oraCFM_Salt, 30, 'o', 'MarkerFaceColor', rgb('light blue'), 'MarkerEdgeColor', rgb('light blue'), 'linewidth', 1.25);
+scatter(plotX(2), oreckCFM_Salt, 30, 'o', 'MarkerFaceColor', rgb('light purple'), 'MarkerEdgeColor', rgb('light purple'), 'linewidth', 1.25);
+scatter(plotX(3), seCFM_Salt, 30, 'o', 'MarkerFaceColor', rgb('light orange'), 'MarkerEdgeColor', rgb('light orange'), 'linewidth', 1.25);
+h1 = scatter(plotX(1), yValues(1), 50, 's', 'MarkerEdgeColor', colors{1}, 'linewidth', 1.25);
+h2 = scatter(plotX(2), yValues(2), 50, 's', 'MarkerEdgeColor', colors{2}, 'linewidth', 1.25);
+h3 = scatter(plotX(3), yValues(3), 50, 's', 'MarkerEdgeColor', colors{3}, 'linewidth', 1.25);
 set(gca, 'ticklabelinterpreter', 'latex');
 
 %Error Bars
-yneg = [oraCFMLow, oreckCFMLow, seCFMLow];
-ypos = [oraCFMUp, oreckCFMUp, seCFMUp];
-errorbar(plotX(1), yValues(1), yValues(1)-yneg(1), ypos(1)-yValues(1), 'color', colors{1});
-errorbar(plotX(2), yValues(2), yValues(2)-yneg(2), ypos(2)-yValues(2), 'color', colors{2});
-errorbar(plotX(3), yValues(3), yValues(3)-yneg(3), ypos(3)-yValues(3), 'color', colors{3});
+% yneg = [oraCFMLow, oreckCFMLow, seCFMLow];
+% ypos = [oraCFMUp, oreckCFMUp, seCFMUp];
+% errorbar(plotX(1), yValues(1), yValues(1)-yneg(1), ypos(1)-yValues(1), 'color', colors{1});
+% errorbar(plotX(2), yValues(2), yValues(2)-yneg(2), ypos(2)-yValues(2), 'color', colors{2});
+% errorbar(plotX(3), yValues(3), yValues(3)-yneg(3), ypos(3)-yValues(3), 'color', colors{3});
 
 ylim([90,350]);
 ylabel('CADR $$(cfm)$$');
 title('Clean Air Delivery Rates');
-legend('Manufacturer Reported', 'Experimentally Determined', 'interpreter', 'latex');
+legend('Salt CADR', 'Smoke CADR', 'interpreter', 'latex');
 
 
 
