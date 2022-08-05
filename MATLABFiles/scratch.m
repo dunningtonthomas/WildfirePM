8485f3e873d23c3b5672bcc45a1958160f83a277
dunningtonthomas <76410702+dunningtonthomas@users.noreply.github.com>
2022-08-05 10:56:09 -0600

    Update scratch.m

--- MATLABFiles/scratch.m
+++ MATLABFiles/scratch.m
@@ -2,6 +2,19 @@
 close all; clear; clc;
 
 
-%% Creating sample log transform plot
+%% Creating color array
+x = linspace(0,10,100);
+y = sin(x);
+colorCell = cell(10,1);
+greenValues = linspace(0,1,10);
 
-sampleNet = googlenet;
+for i = 1:length(colorCell)
+   colorCell{i} = [1 0.75 - greenValues(i) 0];    
+end
+
+figure();
+plot(x,y,'color',colorCell{1})
+hold on
+for i = 1:length(greenValues)
+    plot(x, y-0.1*i, 'color', [1 greenValues(i) 0]);    
+end
